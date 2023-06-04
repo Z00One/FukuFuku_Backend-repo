2effb14497a468366c96a808657a7f2a8e8220ea
Z00One <9000248@g.yju.ac.kr>
2023-06-04 22:58:00 +0900

    refact: Refactor controller to return operation results instead of directly sending responses

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,31 +1,84 @@
-const jwt = require('../modules/jwt');
-
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-module.exports = {
+const { hashing } = require('../utils/hashings');
+const jwt = require('../modules/jwt');
+const status = require('../utils/status');
+const parameterChecker = require('../utils/parameterChecker');
 
-  // 로그인 시 사용자의 id를 바탕으로 토큰 발급
-  signin: async (req, res) => {
-    // 로그인 로직 작성
-    // 로그인 정보가 올바르지 않으면 토큰 없이 response
-    
-    // account정보를 DB에서 조회
+module.exports = {
+  idCheck: async (req, res) => {
+    const { userId } = req.query;
+    const isAuthenticParameter = parameterChecker(userId);
+    if (isAuthenticParameter.isNotMatch) {
+      return isAuthenticParameter.message;
+    };
     const account = await prisma.account.findFirst({
-      where: {
-        userId: userId,
-      },
+      where: { userId: userId },
     });
 
+    if (account) {
+      return status.Conflict;
+    }
+    return status.OK
+  },
+  signin: async (req, res) => {
+    const { userId, userPassword } = req.body;
+    const isAuthenticParameter = parameterChecker(userId, userPassword);
+    if (isAuthenticParameter.isNotMatch) {
+      return isAuthenticParameter.message;
+    };
+    // console.log(userId, userPassword)
+    try {
+      const account = await prisma.account.findFirst({
+        where: {
+          userId: userId
+        },
+      });
+      if (account && account.userPassword === hashing(userPassword)) {
+        // account의 nickname 을 통해 토큰을 생성
+        const jwtToken = await jwt.sign(account);
+        const result = { token: jwtToken.token };
 
-    // userId, userPassword 비교
-    const userId = req.body.userId;
-    const userPassword = req.body.userId;
+        result.isAdmin = account?.isAdmin ? isAdmin : null;
 
-
-    // account의 nickname 을 통해 토큰을 생성
-    const jwtToken = await jwt.sign(account);
-    return { token: jwtToken.token };
-  }
+        return {
+          status: 200,
+          data: result
+        }
+      }
+      else if (!account?.userId) {
+        return status.NotFound;
+      }
+      else return status.Unauthorized;
+    }
+    catch (error) {
+      console.log(error);
+      return status.InternalServerError;
+    }
+  },
+  signup: async (req, res) => {
+    const { userId, userPassword, nickname } = req.body;
+    const isAuthenticParameter = parameterChecker(userId, userPassword, nickname);
+    if (isAuthenticParameter.isNotMatch) {
+      return isAuthenticParameter.message;
+    };
+    // /signup 경로의 POST 요청을 처리할 수 있는 코드
+    try {
+      const account = await prisma.account.create({
+        data: {
+          userId: userId,
+          userPassword: hashing(userPassword),
+          nickname: nickname // 닉네임 중복은 db상에서 UniqueKey로 존재하므로 중복시 account 생성이 불가능함
+        },
+      });
+      if (!account) {
+        return status.Conflict;
+      }
+      return status.OK
+    } catch (error) { // 에러 발생
+      console.log(error);
+      return status.InternalServerError;
+    }
+  },
 }
-

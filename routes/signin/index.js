695fc7944775f7a493d90e84db0af8f0f66b8417
Lainari <108247620+Lainari@users.noreply.github.com>
2023-06-04 20:12:48 +0900

    refact : Modify signin and add signup in routes

--- routes/signin/index.js
+++ routes/signin/index.js
@@ -1,15 +1,13 @@
 // SIGNIN
+const router = require('express').Router();
 const accountController = require('../../controllers/accountController')
-
-module.exports = (express) => {
-  const router = express.Router();
+module.exports = () => {
 
   router.post('/', async(req, res) => {
-
     // 인증 후 jwt 발급
     const jsonWebToken = await accountController.signin(req, res)
-
     res.send(jsonWebToken)
-  })
-  return router
+  });
+
+  return router;
 }

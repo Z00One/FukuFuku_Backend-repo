226762432c94f72a013f3e57249723d309c1b373
Z00One <9000248@g.yju.ac.kr>
2023-06-05 21:23:01 +0900

    feat : Implement numberConverter, parameterChecker, and response formatter `status`
    - `numberConverter`
      - bigint를 int로, int를 bigint로 변환
      - 요청 들어온 값과 실제 테이블의 속성과 자료형 차이로 생기는 문제를 해결
    - `parameterChecker`
      - 요청에 올바른 인자값을 전달했는지 확인
      - 올바르지 않을 경우 status 반환
    - `status`
      - 응답 형식을 반환
      - 응답 형식을 일관성있게 유지

---
+++ utils/parameterChecker.js
@@ -0,0 +1,13 @@
+module.exports = (...args) => {
+  const hasUndefined = args.includes(undefined);
+  if (hasUndefined) {
+    return {
+      isNotMatch: hasUndefined,
+      message: {
+        status: 422,
+        message: 'Requested resource does not match the parameter'
+      }
+    };
+  };
+  return { isNotMatch: hasUndefined };
+}

729d054a326a1d8a85386d122f0f7544638ccfe1
Z00One <9000248@g.yju.ac.kr>
2023-06-14 11:38:05 +0900

    feat : Implement CRUD functionality for comments and functionality to verify the authorship of a comment
    - `writeComment.js`: 댓글작성자, 게시글, 댓글 내용을 기록합니다.
    - `getComments.js`: 특정게시글의 댓글을 가져와 댓글작성자, 댓글내용, 기록 시간 등을 응답합니다.
    - `updateComment.js`: 특정 댓글의 내용을 수정합니다.
    - `deleteComment.js`: 특정 댓글을 삭제합니다.
    - `isCommentOwner.js`: 요청자가 댓글의 작성자인지 확인합니다.

---
+++ controllers/commentController/updateComment.js
@@ -0,0 +1,46 @@
+const NOT_FOUND_ERROR = 'P2025';
+
+module.exports = (prisma, status, parameterChecker) =>
+  async (req, res) => {
+    const { id, comment } = req.body;
+
+    const isAuthenticParameter = parameterChecker(id, comment);
+    if (isAuthenticParameter.isNotMatch) {
+      return isAuthenticParameter.message;
+    };
+    // 댓글 수정, boardNo, nickname, 등록 일자로 구분
+    try {
+      const _id = BigInt(id);
+
+      // boardNo, id 댓글의 comment 수정
+      const _comment = await prisma.comment.update({
+        where: {
+          id: _id
+        },
+        data:{
+          comment: comment
+        }
+      });
+
+      const serializedComment = JSON.stringify(_comment, (key, value) => {
+        if (typeof value === 'bigint') {
+          return Number(value); // BigInt to Number
+        }
+        return value;
+      });
+
+      return {
+        status: status.Created.status,
+        data: serializedComment
+      };
+
+    } catch (error) {
+      console.log(error);
+
+      if (error?.code == NOT_FOUND_ERROR) {
+        return status.NotFound;
+      };
+
+      return status.InternalServerError;
+    };
+  };

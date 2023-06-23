74307b87fb3482fc6e1eb73fbdf53e7e64a5f7f0
Z00One <9000248@g.yju.ac.kr>
2023-06-23 21:05:08 +0900

    refact : Add response for cases where arguments are missing in the code

--- controllers/accountController/isAdmin.js
+++ controllers/accountController/isAdmin.js
@@ -1,17 +1,23 @@
 module.exports = (prisma, status) =>
   async (nickname) => {
+    if (!nickname){
+      return status.Unauthorized;
+    };
+
     try {
       const account = await prisma.account.findFirst({
         where: {
           nickname: nickname
         },
       });
+
       if (!account) {
         return status.NotFound
-      }
-      if (!account.isAdmin) {
+      };
+
+      if (!account?.isAdmin) {
         return status.Unauthorized;
-      }
+      };
 
       return status.OK;
 

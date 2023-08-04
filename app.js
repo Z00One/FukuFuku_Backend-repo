4da5f4b48e0cddf003f41b6f3d0ad6ee0562551b
Z00One <9000248@g.yju.ac.kr>
2023-08-04 15:38:47 +0900

    feat: Enable CORS for client-side requests

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+app.use(cors({
+  origin: process.env.ORIGIN,
+  credentials: true,
+}));
 
 app.use(express.json());
 
 const router = require('./routes')(app);
 
-app.listen(3000, () => {
-  console.log(3000);
-})
+app.listen(process.env.PORT, () => {
+});
